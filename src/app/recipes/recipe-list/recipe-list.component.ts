aed8165668da9d40024c83aacfe4514ad3a0fc54
RamiMZahran <ramimohamedsalah96@gmail.com>
2019-04-16 23:40:54 +0200

    Initial Project Structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','Test','https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
